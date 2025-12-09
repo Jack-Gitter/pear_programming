63d8f43a7bcfdff4686e57fa361b5823985f92db
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-09 17:41:34 -0500

    clean

--- src/main.c
+++ src/main.c
@@ -7,38 +7,81 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <socket_name>\n", argv[0]);
-    return 1;
-  }
+int init_socket(char *filename) {
 
   int sock = socket(PF_LOCAL, SOCK_STREAM, 0);
 
   if (sock < 0) {
-    fprintf(stderr, "failed");
-    return 1;
+    fprintf(stderr, "failed to create socket\n");
+    return -1;
   }
 
   struct sockaddr_un socket_address;
   socket_address.sun_family = AF_LOCAL;
-  char *filename = argv[1];
 
   strncpy(socket_address.sun_path, filename, sizeof(socket_address.sun_path));
   socket_address.sun_path[sizeof(socket_address.sun_path) - 1] = '\0';
 
-  // size_t size = SUN_LEN(&socket_address);
-
-  char *content = "hello world";
-  send(sock, content, strlen(content), 0);
-  /*  if (bind(sock, (struct sockaddr *)&socket_address, size) < 0) {
-      perror("bind");
-      exit(EXIT_FAILURE);
-    }
-
-    close(sock);
-    unlink(filename);*/
-
   return sock;
+}
+
+int cleanup_socket(int socket) {
+  int ret = close(socket);
+  if (ret < 0) {
+    fprintf(stderr, "failed to cleanup socket\n");
+  }
+  return ret;
+}
+
+int send_message(int socket, char *message) {
+
+  size_t len = strlen(message);
+  size_t bytes = 0;
+  while (bytes != len) {
+    int ret = send(socket, message, strlen(message), 0);
+    if (ret < 0) {
+      fprintf(stderr, "failed to send message on socket\n");
+      return ret;
+    }
+    bytes += ret;
+  }
+  return 1;
+}
+
+char *parse_file_path(int argc, char *argv[]) {
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <socket_name>\n", argv[0]);
+    return NULL;
+  }
+
+  return argv[1];
+}
+int main(int argc, char *argv[]) {
+
+  int ret = 0;
+
+  char *file_path = parse_file_path(argc, argv);
+
+  if (file_path == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  int socket = init_socket(file_path);
+
+  if (socket < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  ret = send_message(socket, "hello world");
+
+  if (ret < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  ret = cleanup_socket(socket);
+
+  if (ret != 0) {
+    exit(EXIT_FAILURE);
+  }
 }
